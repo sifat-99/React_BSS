eac1bdde538696e25f3abf162fb1ba1e9068b69a
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-09-07 13:10:19 +0600

    Class-based Components & Context

--- An Alternative Way Of Building Components - Class-based Components/src/App.jsx
+++ An Alternative Way Of Building Components - Class-based Components/src/App.jsx
@@ -1,10 +1,21 @@
-import Users from './components/Users';
+import UserFinder from "./components/UserFinder";
+import UsersContext from "./store/users-context";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Sifat" },
+  { id: "u2", name: "Rifat" },
+  { id: "u3", name: "Sujon" },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
