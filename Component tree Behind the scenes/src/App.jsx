77480f0c9bd30e1192ef3b52c53ac02be093af54
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-09-07 12:19:17 +0600

    feat: reset Counter component state by adding key prop to force remount on initialCount change

--- Component tree Behind the scenes/src/App.jsx
+++ Component tree Behind the scenes/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetClick} />
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
         <Counter initialCount={0} />
       </main>
     </>
