d1013314b785327b68ed49aeb76d732dc8363267
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-09-08 17:57:12 +0600

    React Authentication and demo deply app created and deployed to vercel

---
+++ deploy/build/static/js/793.af690588.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkreact_temp||=[]).push([[793],{793(t,s,e){e.r(s),e.d(s,{default:()=>l,loader:()=>a});var n=e(216),o=e(475);const c="PostList_list__zl9Ee";var r=e(579);const i=function(t){let{posts:s}=t;return(0,r.jsx)("ul",{className:c,children:s.map(t=>(0,r.jsx)("li",{children:(0,r.jsx)(o.N_,{to:t.id.toString(),children:t.title})},t.id))})};const l=function(){const t=(0,n.LG)();return(0,r.jsx)(i,{posts:t})};function a(){return fetch("https://jsonplaceholder.typicode.com/posts")}}}]);
+//# sourceMappingURL=793.af690588.chunk.js.map
---
+++ deploy/src/App.js
@@ -0,0 +1,40 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from './pages/Home';
+import PostPage, { loader as postLoader } from './pages/Post';
+import RootLayout from './pages/Root';
+import { lazy, Suspense } from 'react';
+
+const BlogPage = lazy(() => import('./pages/Blog'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'posts',
+        children: [
+          {
+            index: true, element: <Suspense fallback={<div>Loading...</div>}>
+              <BlogPage />
+            </Suspense>,
+            loader: () => import('./pages/Blog').then((m) => m.loader())
+          },
+          { path: ':id', element: <PostPage />, loader: postLoader },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
